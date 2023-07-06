b13e16b7592ff04bcd54d00f08838e8d131188af
JunhyeokChoi <anchovy1228@naver.com>
2023-07-06 11:17:57 +0900

    refactor 2

--- SimilarityCheckerTest/test.cpp
+++ SimilarityCheckerTest/test.cpp
@@ -3,19 +3,25 @@
 
 using namespace std;
 
-TEST(SimilarityCheckerFixture, 60PointWhenSameLength) {
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
+	SimilarityChecker checker;
+};
+
+TEST_F(SimilarityCheckerFixture, 60PointWhenSameLength) {
 	string input1 = "ABC";
 	string input2 = "DEF";
-	SimilarityChecker checker;
+
 	int point = checker.getLengthPoint(input1, input2);
 
 	EXPECT_EQ(60, point);
 }
 
-TEST(SimilarityCheckerFixture, 0PointWhenBiggerOrSameThanDoubleLength) {
+TEST_F(SimilarityCheckerFixture, 0PointWhenBiggerOrSameThanDoubleLength) {
 	string input1 = "A";
 	string input2 = "AA";
-	SimilarityChecker checker;
+
 	int point = checker.getLengthPoint(input1, input2);
 
 	EXPECT_EQ(0, point);
