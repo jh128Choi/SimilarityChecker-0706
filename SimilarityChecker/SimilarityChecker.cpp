1c7f3642ab5454eb06d4dc9f6afbca129c8c73cc
JunhyeokChoi <anchovy1228@naver.com>
2023-07-06 11:21:19 +0900

    refactor 3

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -6,18 +6,30 @@ using namespace std;
 class SimilarityChecker
 {
 public:
+	bool isLengthDoublUp(int length1, int length2)
+	{
+		if (length1 * 2 <= length2)
+		{
+			return true;
+		}
+		if (length1 >= length2 * 2)
+		{
+			return true;
+		}
+		return false;
+	}
+
 	int getLengthPoint(const string input1, const string input2)
 	{
 		int length1 = input1.size();
 		int length2 = input2.size();
-		if (length1 * 2 <= length2)
-		{
+		if (isLengthDoublUp(length1, length2)) {
 			return 0;
 		}
-		if (length1 >= length2 * 2)
-		{
-			return 0;
-		}
-		return 60;
+
+		return MAX;
 	}
+
+private:
+	static constexpr int MAX = 60;
 };
