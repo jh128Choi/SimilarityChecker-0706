b1c2357490fd2090ed1bf3dbdf2e09f240b7900d
JunhyeokChoi <anchovy1228@naver.com>
2023-07-06 11:40:59 +0900

    refactor 6

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -11,19 +11,24 @@ public:
 		int length1 = input1.size();
 		int length2 = input2.size();
 
+		if (isLengthSame(length1, length2)) {
+			return MAX;
+		}
+
 		if (isLengthDoublUp(length1, length2)) {
 			return 0;
 		}
 
-		int gap = length1 < length2 ? length2 - length1 : length1 - length2;
-		int shortLength = length1 > length2 ? length2 : length1;
-		double rate = gap / (double)shortLength;
-
-		return MAX - (MAX * rate);
+		return getPartialPoint(length1, length2);
 	}
 
 private:
 	static constexpr int MAX = 60;
+
+	bool isLengthSame(int length1, int length2)
+	{
+		return length1 == length2;
+	}
 
 	bool isLengthDoublUp(int length1, int length2)
 	{
@@ -36,5 +41,14 @@ private:
 			return true;
 		}
 		return false;
+	}
+
+	int getPartialPoint(int length1, int length2)
+	{
+		int gap = length1 < length2 ? length2 - length1 : length1 - length2;
+		int shortLength = length1 > length2 ? length2 : length1;
+		double rate = gap / (double)shortLength;
+
+		return MAX - (MAX * rate);
 	}
 };
