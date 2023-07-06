7382371ab79fa89ecc4a6004b59bf802c5576568
JunhyeokChoi <anchovy1228@naver.com>
2023-07-06 11:10:49 +0900

    feature 1

--- SimilarityCheckerTest/test.cpp
+++ SimilarityCheckerTest/test.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
+#include "../SimilarityChecker/SimilarityChecker.cpp"
+
+using namespace std;
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	string input1 = "ABC";
+	string input2 = "DEF";
+	SimilarityChecker checker;
+	int point = checker.getLengthPoint(input1, input2);
+
+	EXPECT_EQ(60, point);
 }
